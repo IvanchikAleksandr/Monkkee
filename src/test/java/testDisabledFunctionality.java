3645467d1c29e2e78386b2ef57910916cf3a915c
IvanchikAleksandr <Ivanchikaleksandr11@gmail.com>
2023-05-02 18:12:09 +0300

    Project Monkkee

--- src/test/java/testDisabledFunctionality.java
+++ src/test/java/testDisabledFunctionality.java
@@ -4,8 +4,11 @@ import org.testng.annotations.Test;
 import static org.testng.AssertJUnit.*;
 
 public class testDisabledFunctionality {
+    private Event disabledfunctionality;
+
     @Test(enabled=false)
     public void testDisabledFunctionality(Event DisabledFunctionality) {
+        disabledfunctionality = DisabledFunctionality;
         // Попытка использования отключенной функциональности.
         // Дополнительная проверка интерфейса и сообщения об отключении.
         assertFalse(DisabledFunctionality.isEnabled());
