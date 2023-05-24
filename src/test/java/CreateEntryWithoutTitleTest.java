db4e6646f72f8855b251ab90eed2008755249ee1
IvanchikAleksandr <Ivanchikaleksandr11@gmail.com>
2023-05-24 22:29:38 +0300

    Project Monkkee

---
+++ src/test/java/CreateEntryWithoutTitleTest.java
@@ -0,0 +1,15 @@
+import Page.DiaryEntry;
+import org.testng.annotations.Test;
+
+    public class CreateEntryWithoutTitleTest extends BaseTest {
+        @Test
+        public void CreateEntryWithoutTitle() {
+            // Создание новой записи в дневнике без названия.
+            DiaryEntry entry = new DiaryEntry();
+
+            // Проверка отображения ошибки.
+            entry.isErrorDisplayed();
+
+        }
+    }
+
---
+++ src/test/java/EditEntryWithoutPermissionTest.java
@@ -0,0 +1,19 @@
+import Page.DiaryEntry;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertTrue;
+
+public class EditEntryWithoutPermissionTest extends BaseTest {
+    @Test
+    public void EditEntryWithoutPermission() {
+        // Создание экземпляра класса, содержащего запись в дневнике без прав для редактирования.
+        DiaryEntry entry = new DiaryEntry(false);
+
+        // Редактирование заголовка и содержания записи.
+        entry.setTitle();
+        entry.setContent();
+
+        // Проверка отображения ошибки.
+        assertTrue(entry.isErrorDisplayed());
+    }
+}
